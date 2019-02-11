8ead2b0527ab1e053424fabacf74e3566d2d5cc0
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-02-11 15:30:38 +0900

    Spreadsheet Main Program add

---
+++ Spreadsheet_Main.scala
@@ -0,0 +1,11 @@
+// スプレッドシートアプリのメインプログラム
+import swing._
+
+object Main extends SimpleSwingApplication {
+
+  def top = new MainFrame {
+    title = "ScalaSheet"
+    contents = new Spreadsheet(100, 26)
+  }
+
+}
