beac60c9b77f633e517a9ce6d20925b2f605d70d
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-02-16 16:24:39 +0900

    add ModelClass and fix SpreadsheetClass. To divide inputData and display

---
+++ Model.scala
@@ -0,0 +1,7 @@
+// セルの座標位置を保持するモデルクラス
+class Model(val height: Int, val width: Int) {
+  case class Cell(row: Int, column: Int)
+  val cells = Array.ofDim[Cell](height, width)
+  for (i <- 0 until height; j <- 0 until width)
+    cells(i)(j) = new Cell(i, j)
+}
--- Spreadsheet.scala
+++ Spreadsheet.scala
@@ -6,12 +6,27 @@ import swing._
 */
 class Spreadsheet(val height: Int, val width: Int) extends ScrollPane {
 
+  val cellModel = new Model(height, width)
+  import cellModel._
+
   // 属性を設定するtableコンポーネント
   val table = new Table(height, width) {
     rowHeight = 25                                      // 表の行の高さ（ポイント指定）
     autoResizeMode = Table.AutoResizeMode.Off           // 表の自動サイズ変更をオフ
     showGrid = true                                     // セルの間にグリッド線を引く
     gridColor = new java.awt.Color(150, 150, 150)       // グリッド線の色をダークグレイに指定
+
+    override def rendererComponent(isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component =
+      if(hasFocus) new TextField(userData(row, column))
+      else
+        new Label(cells(row)(column).toString) {
+          xAlignment = Alignment.Right
+        }
+
+    def userData(row: Int, column: Int): String = {
+      val v = this(row, column)
+      if(v == null) "" else v.toString
+    }
   }
 
   // 行番号のヘッダー
