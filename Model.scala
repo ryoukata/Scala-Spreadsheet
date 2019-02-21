b79207c4b9d0c0f776476a088f2651bf19788316
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-02-21 15:53:49 +0900

    chane ModelClass to integrate FormulaParserClass

--- Model.scala
+++ Model.scala
@@ -2,6 +2,10 @@
 class Model(val height: Int, val width: Int) {
   case class Cell(row: Int, column: Int)
   val cells = Array.ofDim[Cell](height, width)
+  val formula: Formula = Empty                      // スプレッドシートにパーサを統合
+
+  override def toString = formula.toString
   for (i <- 0 until height; j <- 0 until width)
     cells(i)(j) = new Cell(i, j)
+
 }
